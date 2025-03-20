103e263c707d687fbadf999d2b6a6522e7dbc200
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-03-20 11:14:24 +0200

    feat: Add video upload and S3 integration

--- backend/src/video/video.controller.ts
+++ backend/src/video/video.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 
@@ -13,16 +14,17 @@ import { VideoService } from './video.service';
 import { UploadVideoDto } from './dto/upload-video.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiConsumes } from '@nestjs/swagger';
+import { FileUploadService } from './file-upload.service';
 
 @Controller('videos')
 export class VideoController {
-  constructor(private readonly videoService: VideoService) {}
+  constructor(private readonly videoService: VideoService,private readonly fileUploadService:FileUploadService) {}
 
   @Post()
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(FileInterceptor('file'))
-  async create(@Body() data: UploadVideoDto) {
-    return "video"
+  async create(@Body() data: UploadVideoDto,@UploadedFile()file:Express.Multer.File) {
+    await this.fileUploadService.uploadFile(file.buffer,data.name)
   }
 }
 
--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { FileUploadService } from './file-upload.service';
 import { VideoController } from './video.controller';
 import { VideoService } from './video.service';
 
 @Module({
   imports: [],
   controllers: [VideoController,],
-  providers: [VideoService,],
+  providers: [FileUploadService, VideoService, PrismaService],
   exports: []
 })
 export class VideoModule { }
