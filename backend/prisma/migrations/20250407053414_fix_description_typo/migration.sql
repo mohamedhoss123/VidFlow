9ef17dfc86074c69361fb13bb29a482e29109c77
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2025-06-28 18:49:30 +0300

    feat(database): initialize PostgreSQL migration with user, video, and related tables

--- backend/prisma/migrations/20250407053414_fix_description_typo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `discription` on the `Video` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Video` DROP COLUMN `discription`,
-    ADD COLUMN `description` VARCHAR(191) NULL;
