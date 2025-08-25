0f7fe75add377b1b33a01ce123f1d920ee81148b
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-08-25 09:48:21 +0300

    feat(main-service): implement create video gRPC call and update makeReady to include video length

--- main-service/src/video/service/video.service.ts
+++ main-service/src/video/service/video.service.ts
@@ -4,13 +4,32 @@ import { UpdateVideoDto } from '../dto/update-video.dto';
 import { CreateVideoRequest, VideoReadyRequest } from 'src/common/proto/video';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { VideoResponseDto } from '../dto/video.dto';
-import { VideoStatus } from '@prisma/client';
+import { VideoStatus, VideoVisibility } from '@prisma/client';
+import { uuidv7 } from 'uuidv7';
 
 @Injectable()
 export class VideoService {
   constructor(private readonly prismaService:PrismaService){}
-  create(createVideoDto: CreateVideoRequest) {
-    // this.prismaService.video.create({})
+
+  async create(createVideoDto: CreateVideoRequest) {
+    const id = uuidv7()
+    await this.prismaService.video.create({data:{
+      id,
+      name:createVideoDto.url,
+      description:createVideoDto.description,
+      user_id:createVideoDto.userId,
+      likes_count:0,
+      comments_count:0,
+      visibility:VideoVisibility.public,
+      status:VideoStatus.PROCESSING,
+      created_at:new Date(),
+      qualities:{
+        create:[]
+      }
+    }})
+    return {
+      videoId:id
+    }
   }
 
   getVideoWithQualities( videoId: string) {
@@ -29,7 +48,8 @@ export class VideoService {
     await this.prismaService.video.update({
       where:{id:videoReadyRequest.videoId},
       data:{
-        status:VideoStatus.READY
+        status:VideoStatus.READY,
+        length:videoReadyRequest.length
       }
     })
   }
