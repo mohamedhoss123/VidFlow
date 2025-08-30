fd9c2f8a5ff2bb3349c7214e3e0659b37a4d62a1
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-08-30 12:59:05 +0300

    fix(main-service): remove password from endpoint response

--- main-service/src/video/service/video.service.ts
+++ main-service/src/video/service/video.service.ts
@@ -65,7 +65,11 @@ export class VideoService {
         created_at: 'desc'
       },
       include: {
-        user: true,
+        user: {
+          select:{
+            name:true,
+          }
+        },
       }
     })
   
