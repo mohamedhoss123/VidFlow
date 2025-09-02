65867438d25be203c3ac51bad973f6df165be14d
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-09-02 11:30:39 +0300

    fix(main-service): exclude unready videos from retrieval

--- main-service/src/video/service/video.service.ts
+++ main-service/src/video/service/video.service.ts
@@ -1,83 +1,92 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { CreateVideoDto } from '../dto/create-video.dto';
-import { UpdateVideoDto } from '../dto/update-video.dto';
-import { CreateVideoRequest, VideoReadyRequest } from 'src/common/proto/video';
-import { PrismaService } from 'src/prisma/prisma.service';
-import {  VideoResponseDto } from '../dto/video.dto';
-import { VideoStatus, VideoVisibility } from '@prisma/client';
-import { uuidv7 } from 'uuidv7';
-import { VideoPaginationDto } from '../dto/video-pagination.dto';
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { CreateVideoDto } from "../dto/create-video.dto";
+import { UpdateVideoDto } from "../dto/update-video.dto";
+import { CreateVideoRequest, VideoReadyRequest } from "src/common/proto/video";
+import { PrismaService } from "src/prisma/prisma.service";
+import { VideoResponseDto } from "../dto/video.dto";
+import { VideoStatus, VideoVisibility } from "@prisma/client";
+import { uuidv7 } from "uuidv7";
+import { VideoPaginationDto } from "../dto/video-pagination.dto";
 
 @Injectable()
 export class VideoService {
-  constructor(private readonly prismaService:PrismaService){}
+  constructor(private readonly prismaService: PrismaService) {}
 
   async create(createVideoDto: CreateVideoRequest) {
-    const id = uuidv7()
-    await this.prismaService.video.create({data:{
-      id,
-      name:createVideoDto.url,
-      description:createVideoDto.description,
-      user_id:createVideoDto.userId,
-      likes_count:0,
-      comments_count:0,
-      visibility:VideoVisibility.public,
-      status:VideoStatus.PROCESSING,
-      created_at:new Date(),
-      qualities:{
-        create:[]
-      }
-    }})
+    const id = uuidv7();
+    await this.prismaService.video.create({
+      data: {
+        id,
+        name: createVideoDto.url,
+        description: createVideoDto.description,
+        user_id: createVideoDto.userId,
+        likes_count: 0,
+        comments_count: 0,
+        visibility: VideoVisibility.public,
+        status: VideoStatus.PROCESSING,
+        created_at: new Date(),
+        qualities: {
+          create: [],
+        },
+      },
+    });
     return {
-      videoId:id
-    }
+      videoId: id,
+    };
   }
 
-  getVideoWithQualities( videoId: string) {
-    return this.prismaService.video.findUnique({ where: { id: videoId } ,include:{qualities:true}});
+  getVideoWithQualities(videoId: string) {
+    return this.prismaService.video.findUnique({
+      where: { id: videoId },
+      include: { qualities: true },
+    });
   }
 
   async makeVideoReady(videoReadyRequest: VideoReadyRequest) {
-    await this.prismaService.videoQuality.createMany({data:videoReadyRequest.quality.map((quality)=>{
-      return {
-        id:quality.id,
-        quality:quality.quality,
-        video_id:videoReadyRequest.videoId,
-        objectId:quality.objectId,
-      }
-    })})
+    await this.prismaService.videoQuality.createMany({
+      data: videoReadyRequest.quality.map((quality) => {
+        return {
+          id: quality.id,
+          quality: quality.quality,
+          video_id: videoReadyRequest.videoId,
+          objectId: quality.objectId,
+        };
+      }),
+    });
     await this.prismaService.video.update({
-      where:{id:videoReadyRequest.videoId},
-      data:{
-        status:VideoStatus.READY,
-        length:videoReadyRequest.length
-      }
-    })
+      where: { id: videoReadyRequest.videoId },
+      data: {
+        status: VideoStatus.READY,
+        length: videoReadyRequest.length,
+      },
+    });
   }
 
-  async getVideos(videoPaginationDto:VideoPaginationDto){
+  async getVideos(videoPaginationDto: VideoPaginationDto) {
     const videos = await this.prismaService.video.findMany({
       take: videoPaginationDto.limit,
       skip: videoPaginationDto.cursor ? 1 : 0,
-      cursor: videoPaginationDto.cursor ? { id: videoPaginationDto.cursor } : undefined,
-      where:{visibility:VideoVisibility.public},
+      cursor: videoPaginationDto.cursor
+        ? { id: videoPaginationDto.cursor }
+        : undefined,
+      where: { visibility: VideoVisibility.public, status: VideoStatus.READY },
       orderBy: {
-        created_at: 'desc'
+        created_at: "desc",
       },
       include: {
         user: {
-          select:{
-            name:true,
-          }
+          select: {
+            name: true,
+          },
         },
-      }
-    })
-  
-    const nextCursor = videos.length > 0 ? videos[videos.length - 1].id : null
-  
+      },
+    });
+
+    const nextCursor = videos.length > 0 ? videos[videos.length - 1].id : null;
+
     return {
       videos,
-      nextCursor
-    }
+      nextCursor,
+    };
   }
 }
