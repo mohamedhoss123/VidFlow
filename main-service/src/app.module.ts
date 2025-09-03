895c14a18567082695c5aef00da2e44580f4f8fa
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-09-03 16:54:31 +0300

    feat(main-service): implement subscription CRUD endpoints

--- main-service/src/app.module.ts
+++ main-service/src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { VideoModule } from './video/video.module';
-import { AuthModule } from './auth/auth.module';
-import { ThumbnailModule } from './thumbnail/thumbnail.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaModule } from "./prisma/prisma.module";
+import { VideoModule } from "./video/video.module";
+import { AuthModule } from "./auth/auth.module";
+import { ThumbnailModule } from "./thumbnail/thumbnail.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [PrismaModule, VideoModule, AuthModule, ThumbnailModule],
+  imports: [
+    PrismaModule,
+    VideoModule,
+    AuthModule,
+    ThumbnailModule,
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
